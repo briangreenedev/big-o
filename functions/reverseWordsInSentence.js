f74dd9a69f95baec702db670a6cdd918d9b39108
briangreenedev <brian@cloversoftware.net>
2020-06-15 17:13:57 -0600

    Redid reverseWordsInSentence.js for practice.

--- functions/reverseWordsInSentence.js
+++ functions/reverseWordsInSentence.js
@@ -1,19 +1,19 @@
 module.exports = (str) => {
 	let result = ''
-	let currentWordEnd = str.length - 1
+	let endOfCurrentWord = str.length - 1
 	let i = str.length - 1
 
 	while(i >= 0) {
 		if(str[i] === ' ') {
 			if(result.length > 0)
 				result += ' '
-			result += str.substring(i + 1, currentWordEnd + 1)
-			currentWordEnd = i - 1
+			result += str.substring(i + 1, endOfCurrentWord + 1)
+			endOfCurrentWord = i - 1
 		}
 		i--
 	}
 
-	let firstWord = str.substring(0, currentWordEnd + 1)
+	const firstWord = str.substring(0, endOfCurrentWord + 1)
 	if(result.length > 0)
 		result += ' '
 	result += firstWord
